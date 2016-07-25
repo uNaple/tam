b63ad42231da1bb3ee874529a6546d62266c5a30
uNaple <retkonstantin@gmail.com>
2016-07-25 17:38:36 +0300

    Отдаю с сервера ассоциативный массив с дотупом по id. Навешиваю обработку на клик сразу при динамическом создании списка задач

--- public/js/main.js
+++ public/js/main.js
@@ -3,34 +3,34 @@ var xhr = new XMLHttpRequest();
 
 $(document).ready(function() {
 
+	var allTasks = new Array(),//здесь хранятся задачи, потом редиска будет
+		myTasks = new Array(),
+		listStatus = new Array(),
+		listTypes = new Array(),
+		listUsers = new Array();
+		getExtra();
+
 //======== Добавление задачи
 	$('#buttonAddTask1').on('click', function(event) {
 		$('#listTasks').prepend('<li class="flexrow" style=""><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><input value="" type="checkbox"><span class="text taskedit" contenteditable="true" >New task' );
+		var task = {name: 'New Task'};
+		showInfo(task);
 	});
+
 	$('#buttonAddTask2').on('click', function(event) {
 		$('#listTasks').append('<li class="flexrow" style=""><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><input value="" type="checkbox"><span class="text taskedit" contenteditable="true" >New task' );
 	});
 
-	$('.todo-list').click(function() {
-		console.debug('on task click');
-		getExtra();
-		$('#panelTaskEdit').show();
-	})
-
-	$('#tableButtonCancel').click(function(event) {
-		console.debug(event);
-		Custombox.close();
-	});
-
 	$('#buttonTaskAdd').click(function(event) {
-		console.debug($('#panelTaskEdit').html());
-
-		// var data = $('#tableAdd tr td input');
-		// var task = new Object();
-		// for(var i = 0; i < (data.length-2); i++) {
-		// 	task[$(data[i]).attr('name')] = $.trim($(data[i]).val());
-		// 	console.debug(task);
-		// }
+		var data = $('#panelTaskEdit select');
+		console.debug($('select#listUsers').val(), $('select#listUsers').attr('name'));
+		console.debug($('select#listTypes').val());
+		console.debug($('select#listStatus').val());
+		var task = new Object();
+		for(var i = 0; i < data.length; i++) {
+			task[data[i].name] = $('select#'+data[i].id).val();
+		}
+		console.debug(task);
 		// event.preventDefault();
 		// $.ajax({
 		// 	url: 'addTask',
@@ -39,19 +39,14 @@ $(document).ready(function() {
 		// 	success: function(result) {
 		// 			result = JSON.parse(result);
 		// 			alert(result)
-			// 		console.debug(result);
+		// 			console.debug(result);
 		// 		}
 		// });
-		// Custombox.close();
 	});
 
 //======== Отображение задач
-	var allTasks = new Array(),//здесь хранятся задачи, потом редиска будет
-			myTasks = new Array(),
-			listStatus = new Array(),
-			listTypes = new Array(),
-			listUsers = new Array();
 
+//вот эту ебалу свернуть в одну функцию, внутри которой и делать выборку по тому, что необходимо отобразить
 	$('#buttonMyTasks').click(function() {
 			console.debug('myTasks func');
 			var params = 'director=' + encodeURIComponent(679); //тут id из сессии
@@ -97,14 +92,49 @@ $(document).ready(function() {
 
 //======== Вспомогательные функции
 
-//Отображение листа с задачами. Перевести его в таблицу!
+	function showInfo(task) {
+		// console.debug(arr[id]);
+		for(var key in task) {
+			if(key !== 'null') {
+				if(key === 'name') {
+					$('#panelTaskEdit #name').val(task.name);
+				}
+				if(key === 'director') {
+					// console.debug($('#panelTaskEdit [name="director"]').val(myTasks[id].director));
+					// console.debug($('#panelTaskEdit [name="director"]').val());
+					// $('#listUsers [id='+myTasks[id].director+']').attr('selected', 'selected');
+					$('#listUsers [id='+task.director+']').val();
+				}
+			}
+		}
+		$('#panelTaskEdit').show();
+	};
+
+//Отображение листа с задачами.
 	function showAll(arr) {
 		$('#listTasks').empty();
-		for(var i = 0; i <  arr.length; i++) {
-			$('#listTasks').append('<li class="flexrow" style=""><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><input value="" type="checkbox"><span class="text taskedit" contenteditable="true" >' + arr[i].name);
+		for(var key in arr) {
+			var elem = $('<li class="flexrow" style="" data-id="' + arr[key].id +'"><span class="handle ui-sortable-handle"><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i><i class="fa fa-ellipsis-v"></i></span><input value="" type="checkbox"><span class="text taskedit" contenteditable="true" >' + arr[key].name + '</span></li>');
+			$('#listTasks').append(elem);
+
+			elem.click(function() {
+				console.debug('click on task with id:', $(this).attr('data-id') );
+				showInfo(arr[$(this).attr('data-id')]);
+			})
 		}
-	}
-//Отображение доп инфы
+	};
+
+	// $('.todo-list').click(function() {
+	// 	console.debug('on task click');//надо вытащить ид у задачи по которой был клик
+	// 	// console.debug(event.currentTarget.children[]);
+	// 	// console.debug($(this).children().text());
+	// 	// getExtra();//вынести отдельно, чтоб грузилось при входе на страницу в невидимом режиме и хранить это
+	// 	// $('#')
+	// 	// showInfo('5');
+	// 	// $('#panelTaskEdit').show();
+	// })
+
+//Отображение инфы
 	function showExtra() {
 		$("#listUsers").select2({
 			data: listUsers
@@ -115,7 +145,7 @@ $(document).ready(function() {
 		$("#listStatus").select2({
 			data: listStatus
 		});
-	}
+	};
 
 //Подгрузка списков
 	function getExtra() {
@@ -144,17 +174,17 @@ $(document).ready(function() {
 				showExtra();
 			})
 		}
-	}
+	};
 
 //Проверка массива всех задач
 	function checkExist(arr) {//тут проверять вероятно будем из редиски
-		console.debug(arr);
+		// console.debug(arr);
 		if(arr.length !== 0) {
 			return true;
 		} else {
 			console.debug('arr.length:', arr.length);
 			return false;
 		}
-	}
-});
+	};
 
+})
